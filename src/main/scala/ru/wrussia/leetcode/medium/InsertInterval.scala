a4a68aa03e5d4d07eecbe89b2740175049b33bb1
Konstantin Kostikov <kkostikov@beeline.ru>
2022-03-29 23:22:29 +0300

    Insert Interval

--- src/main/scala/ru/wrussia/leetcode/medium/InsertInterval.scala
+++ src/main/scala/ru/wrussia/leetcode/medium/InsertInterval.scala
@@ -6,35 +6,31 @@ import scala.collection.mutable.ArrayBuffer
 
 object InsertInterval {
 
-  def intersects(a: Array[Int], b: Array[Int]): Boolean = (a,b) match {
-    case (Array(a1,b1),Array(a2,b2)) => Intervals.intersects(a1,b1,a2,b2)
+  def intersects(a: Array[Int], b: Array[Int]): Boolean = (a, b) match {
+    case (Array(a1, b1), Array(a2, b2)) => Intervals.intersects(a1, b1, a2, b2)
     case _ => false
   }
 
   def insert(intervals: Array[Array[Int]], newInterval: Array[Int]): Array[Array[Int]] = {
     val result: ArrayBuffer[Array[Int]] = ArrayBuffer[Array[Int]]()
-    var tmpInterval: Array[Int] = null
-    var flag = "before"
+    var i = 0
 
-    if (intervals.isEmpty) return Array(newInterval)
-
-    if (intervals.nonEmpty && newInterval(1) < intervals.head(0)) result.append(newInterval)
-
-    for (interval <- intervals) {
-      if (intersects(interval, newInterval)) {
-        if (tmpInterval == null) tmpInterval = Array(math.min(interval(0), newInterval(0)), math.max(interval(1), newInterval(1)))
-        else tmpInterval = Array(tmpInterval(0), math.max(interval(1), tmpInterval(1)))
-        flag = "in_progress"
-      } else if (flag == "in_progress")  {
-        result.append(tmpInterval)
-        flag = "finished"
-      }
-      if (flag == "before" || flag == "finished") {
-        result.append(interval)
-      }
+    while (i < intervals.length && intervals(i)(1) < newInterval(0)) {
+      result.append(intervals(i))
+      i += 1
     }
 
-    if (intervals.nonEmpty && intervals.last(1) < newInterval(0)) result.append(newInterval)
+    var tmpInterval = newInterval
+    while (i < intervals.length && intersects(intervals(i), newInterval)) {
+      tmpInterval = Array(math.min(tmpInterval(0), intervals(i)(0)), math.max(tmpInterval(1), intervals(i)(1)))
+      i += 1
+    }
+    result.append(tmpInterval)
+
+    while (i < intervals.length) {
+      result.append(intervals(i))
+      i += 1
+    }
 
     result.toArray
   }
--- src/test/scala/ru/wrussia/leetcode/medium/TestInsertIntervals.scala
+++ src/test/scala/ru/wrussia/leetcode/medium/TestInsertIntervals.scala
@@ -6,41 +6,57 @@ class TestInsertIntervals extends WordSpecLike with Matchers {
 
 
   "case 1" in {
-    val intervals = Array(Array(1,2),Array(3,5),Array(6,7),Array(8,10),Array(12,16))
-    val newInterval = Array(4,8)
-    val expected = Array(Array(1,2),Array(3,10),Array(12,16))
+    val intervals = Array(Array(1, 2), Array(3, 5), Array(6, 7), Array(8, 10), Array(12, 16))
+    val newInterval = Array(4, 8)
+    val expected = Array(Array(1, 2), Array(3, 10), Array(12, 16))
 
     InsertInterval.insert(intervals, newInterval) shouldBe expected
   }
 
   "case 2" in {
-    val intervals = Array(Array(1,3),Array(6,9))
-    val newInterval = Array(2,5)
-    val expected = Array(Array(1,5),Array(6,9))
+    val intervals = Array(Array(1, 3), Array(6, 9))
+    val newInterval = Array(2, 5)
+    val expected = Array(Array(1, 5), Array(6, 9))
 
     InsertInterval.insert(intervals, newInterval) shouldBe expected
   }
 
   "case 3" in {
     val intervals: Array[Array[Int]] = Array()
-    val newInterval = Array(2,5)
-    val expected = Array(Array(2,5))
+    val newInterval = Array(2, 5)
+    val expected = Array(Array(2, 5))
 
     InsertInterval.insert(intervals, newInterval) shouldBe expected
   }
 
   "case 4" in {
-    val intervals: Array[Array[Int]] = Array(Array(1,5))
-    val newInterval = Array(6,8)
-    val expected = Array(Array(1,5), Array(6,8))
+    val intervals: Array[Array[Int]] = Array(Array(1, 5))
+    val newInterval = Array(6, 8)
+    val expected = Array(Array(1, 5), Array(6, 8))
 
     InsertInterval.insert(intervals, newInterval) shouldBe expected
   }
 
   "case 5" in {
-    val intervals: Array[Array[Int]] = Array(Array(6,8))
-    val newInterval = Array(1,5)
-    val expected = Array(Array(1,5), Array(6,8))
+    val intervals: Array[Array[Int]] = Array(Array(6, 8))
+    val newInterval = Array(1, 5)
+    val expected = Array(Array(1, 5), Array(6, 8))
+
+    InsertInterval.insert(intervals, newInterval) shouldBe expected
+  }
+
+  "case 6" in {
+    val intervals: Array[Array[Int]] = Array(Array(1, 5))
+    val newInterval = Array(2, 3)
+    val expected = Array(Array(1, 5))
+
+    InsertInterval.insert(intervals, newInterval) shouldBe expected
+  }
+
+  "case 7" in {
+    val intervals: Array[Array[Int]] = Array(Array(3, 5), Array(11,12))
+    val newInterval = Array(6, 6)
+    val expected = Array(Array(3, 5), Array(6, 6), Array(11, 12))
 
     InsertInterval.insert(intervals, newInterval) shouldBe expected
   }

80f5144fe9558d66f0db66ee5ec59de3c0b63da8
Konstantin Kostikov <kkostikov@beeline.ru>
2022-03-30 22:27:21 +0300

    Maximum Subarray

--- src/main/scala/ru/wrussia/leetcode/easy/MaximumSubarray.scala
+++ src/main/scala/ru/wrussia/leetcode/easy/MaximumSubarray.scala
@@ -1,7 +1,13 @@
 package ru.wrussia.leetcode.easy
 
+import scala.collection.mutable.ListBuffer
+
 object MaximumSubarray {
   def maxSubArray(nums: Array[Int]): Int = {
-    6
+    nums.tail.foldLeft((nums.head, nums.head)) { case ((globalMax: Int, currSum: Int), curr: Int) =>
+      val cSum = curr + (if (currSum > 0) currSum else 0)
+      val gMax = if (globalMax < cSum) cSum else globalMax
+      (gMax, cSum)
+    }._1
   }
 }
--- src/test/scala/ru/wrussia/leetcode/easy/TestMaximumSubarray.scala
+++ src/test/scala/ru/wrussia/leetcode/easy/TestMaximumSubarray.scala
@@ -3,7 +3,13 @@ package ru.wrussia.leetcode.easy
 import org.scalatest.{Matchers, WordSpecLike}
 
 class TestMaximumSubarray extends WordSpecLike with Matchers {
-  "simple" in {
+  "case 1" in {
     MaximumSubarray.maxSubArray(Array(-2,1,-3,4,-1,2,1,-5,4)) shouldBe 6
+  }
+  "case 2" in {
+    MaximumSubarray.maxSubArray(Array(5,4,-1,7,8)) shouldBe 23
+  }
+  "case 3" in {
+    MaximumSubarray.maxSubArray(Array(-2,-3,-4,-5,-1)) shouldBe -1
   }
 }

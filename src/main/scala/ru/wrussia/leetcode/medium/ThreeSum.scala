a2e6ff1996be8326fc8dd42937ab80bc1c3f98e9
Konstantin Kostikov <kkostikov@beeline.ru>
2022-04-01 23:42:37 +0300

    3Sum

--- src/main/scala/ru/wrussia/leetcode/medium/ThreeSum.scala
+++ src/main/scala/ru/wrussia/leetcode/medium/ThreeSum.scala
@@ -6,25 +6,28 @@ import scala.util.Sorting
 
 object ThreeSum {
 
-  def binarySearch(sortedSeq: Seq[Int], elem: Int, start: Int = 0): Int = {
-    var left = start
-    var right = sortedSeq.length
-
-    while (right - left > 1) {
-      val mid = (right - left) / 2 + left
-      if (sortedSeq(mid) > elem) right = mid
-      else left = mid
-    }
-    if (left < sortedSeq.length && sortedSeq(left) == elem) left else -1
-  }
-
   def threeSum(nums: Array[Int]): List[List[Int]] = {
     Sorting.quickSort(nums)
-    val resultSet = mutable.Set[List[Int]]()
-    for (i <- nums.indices if nums(i) <= 0; j <- i + 1 until nums.length) {
-      if (binarySearch(nums, -(nums(i) + nums(j)), j + 1) > 0)
-        resultSet.add(List(nums(i), nums(j), -(nums(i) + nums(j))))
+    val result = mutable.Set[List[Int]]()
+
+    for (i <- 0 until nums.length - 2
+         if nums(i) <= 0 &&
+           (nums(i) == 0 || i == 0 || nums(i) != nums(i - 1))) {
+      var left = i + 1
+      var right = nums.length - 1
+      while (left < right) {
+        val currSum = nums(i) + nums(left) + nums(right)
+        currSum match {
+          case 0 =>
+            result += List(nums(i), nums(left), nums(right))
+            left += 1
+            right -= 1
+          case _ if currSum < 0 => left += 1
+          case _ if currSum > 0 => right -= 1
+        }
+      }
     }
-    resultSet.toList
+
+    result.toList
   }
 }
--- src/test/scala/ru/wrussia/leetcode/medium/Test3Sum.scala
+++ src/test/scala/ru/wrussia/leetcode/medium/Test3Sum.scala
@@ -4,9 +4,15 @@ import org.scalatest.{Matchers, WordSpecLike}
 
 class Test3Sum extends WordSpecLike with Matchers {
   "case 1" in {
-    ThreeSum.threeSum(Array(3,1,4,1,5,-6)) shouldBe List(List(-6,1,5))
+    ThreeSum.threeSum(Array(3,1,4,1,5,-6)) should contain theSameElementsAs List(List(-6,1,5))
   }
   "case 2" in {
     ThreeSum.threeSum(Array(0,0,0)) shouldBe List(List(0,0,0))
+  }
+  "case 3" in {
+    ThreeSum.threeSum(Array(-1,0,1,2,-1,-4)) should contain theSameElementsAs List(List(-1,-1,2), List(-1, 0, 1))
+  }
+  "case 4" in {
+    ThreeSum.threeSum(Array(0,0,0,0)) shouldBe List(List(0,0,0))
   }
 }
